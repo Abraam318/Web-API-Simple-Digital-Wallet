fdeaedd4223cc7e7ab3558c575965110562d94af
Abraam Adel Demitry <126262425+Abraam318@users.noreply.github.com>
2024-07-22 16:42:05 +0300

    Transaction Service

---
+++ Services/TransactionService.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_Simple_Digital_Wallet.Services
+{
+    public class TransactionService
+    {
+        private readonly TransactionRepository _transactionRepository;
+        private readonly UserRepository _userRepository;
+
+        public TransactionService(TransactionRepository transactionRepository, UserRepository userRepository)
+        {
+            _transactionRepository = transactionRepository;
+            _userRepository = userRepository;
+        }
+
+        public async Task<IEnumerable<Transaction>> GetAllTransactionsAsync()
+        {
+            return await _transactionRepository.GetAllTransactionsAsync();
+        }
+
+        public async Task<Transaction> GetTransactionByIdAsync(int id)
+        {
+            return await _transactionRepository.GetTransactionByIdAsync(id);
+        }
+
+        public async Task AddTransactionAsync(Transaction transaction)
+        {
+            await _transactionRepository.AddTransactionAsync(transaction);
+        }
+
+        public async Task UpdateTransactionAsync(Transaction transaction)
+        {
+            await _transactionRepository.UpdateTransactionAsync(transaction);
+        }
+
+        public async Task DeleteTransactionAsync(int id)
+        {
+            await _transactionRepository.DeleteTransactionAsync(id);
+        }
+}
